c21d1ef85e29985dbcc59b338db32fcc6bb94d09
Reuben <reubendrummond@gmail.com>
2022-05-12 16:46:38 +1000

    GitHub page config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || "",
   reactStrictMode: false,
   images: {
     domains: ["lh3.googleusercontent.com", "upload.wikimedia.org"],
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,17 @@
 import "@styles/globals.css";
 import { FC, useEffect } from "react";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 const MyApp: FC<AppProps> = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link rel="shortcut icon" href={"/favicon.ico"} />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 };
 
 export default MyApp;
