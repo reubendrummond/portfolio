ee938fb3dfb07d719c5f3fd1367b8f4a947b7282
Reuben <reubendrummond@gmail.com>
2022-05-22 12:59:33 +1000

    Changing css to fit content on mobile

--- components/Sections/ILike/ILike.tsx
+++ components/Sections/ILike/ILike.tsx
@@ -13,16 +13,16 @@ export const ILike = (props: SectionProps) => {
   // };
   // this causes an error I believe
   // const sizesClass = `w-[${sizes.sm}px] md:w-[${sizes.md}px] lg:w-[${sizes.lg}px]`;
-  const sizesClass = "w-[250px] md:w-[280px] lg:w-[350px] xl:w-[350px]";
+  const sizesClass = "w-[200px] md:w-[280px] lg:w-[350px] xl:w-[350px]";
 
   return (
     <section
       id={props.id}
-      className="w-full h-full px-8 py-12 md:pt-16 flex flex-col gap-y-12 md:gap-y-0 items-center "
+      className="w-full h-full px-8 py-12 md:pt-16 flex flex-col md:gap-y-0 items-center "
     >
       <HighlightedHeading>I like...</HighlightedHeading>
       <div className="flex flex-col h-full md:flex-row items-center w-fit justify-around md:gap-x-16 lg:gap-x-32">
-        <div className="flex flex-col gap-y-4">
+        <div className="flex flex-col gap-y-4 items-center">
           <div className="flex flex-col justify-center items-center gap-y-1">
             <Typing content="software" className="font-mono text-2xl md:3xl" />
             <p>Frontend and backend web dev.</p>
