7d9370c7f87859506be5632acf53e412e2c2e72b
Reuben <reubendrummond@gmail.com>
2022-07-02 20:09:10 +1000

    switch to next-themes for light/dark modes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,16 +2,19 @@ import "@styles/globals.css";
 import { FC } from "react";
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import { ThemeProvider } from "next-themes";
 
 const MyApp: FC<AppProps> = ({ Component, pageProps }) => {
   return (
     <>
       <Head>
         <title>Portfolio</title>
-        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="shortcut icon" href="/images/favicon.ico" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
-      <Component {...pageProps} />
+      <ThemeProvider attribute="class">
+        <Component {...pageProps} />
+      </ThemeProvider>
     </>
   );
 };
